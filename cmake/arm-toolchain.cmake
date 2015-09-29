038a09ea4704dd3ec36f871fbb82f3a7851a09ad
Cruel <machin3@gmail.com>
2015-09-29 12:52:42 -0400

    Fix travis/cmake scripts

--- cmake/arm-toolchain.cmake
+++ cmake/arm-toolchain.cmake
@@ -7,9 +7,13 @@ SET(CMAKE_CXX_COMPILER ${DEVKITARM}/bin/arm-none-eabi-g++)
 SET(CMAKE_SIZE ${DEVKITARM}/bin/arm-none-eabi-size)
 SET(CMAKE_OBJCOPY ${DEVKITARM}/bin/arm-none-eabi-objcopy)
 
+set(CMAKE_FIND_ROOT_PATH ${DEVKITARM} ${DEVKITPRO} ${DEVKITPRO}/portlibs/3ds ${DEVKITPRO}/portlibs/armv6k)
+#set(CMAKE_INCLUDE_PATH ${DEVKITPRO}/portlibs/3ds/include ${DEVKITPRO}/portlibs/armv6k/include)
+#set(CMAKE_LIBRARY_PATH ${DEVKITPRO}/portlibs/3ds/lib ${DEVKITPRO}/portlibs/armv6k/lib)
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
 # programs in the host environment
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
